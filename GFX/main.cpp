c0c33a2edc2b1332aba3eb658e88ac78da424b97
Алексей Олохтонов <aolo2@yandex.ru>
2017-01-30 20:07:14 +0300

    glfw 3.3 -> 3.2

--- GFX/main.cpp
+++ GFX/main.cpp
@@ -74,7 +74,7 @@ int make_shaders(const char* vs_src, const char* fs_src, GLuint* shaderProgram) 
 int main() {
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
     

4a09b8f98ba961d0007c416d77f6091e92568811
Алексей Олохтонов <aolo2@yandex.ru>
2017-02-18 22:11:22 +0300

    Add files via upload

---
+++ GFX/Phong lighted sphere/Camera.cpp
@@ -0,0 +1,67 @@
+#include "Camera.h"
+#include <GLFW/glfw3.h>
+
+Camera::Camera(glm::vec3 pos, glm::vec3 target, glm::vec3 up) {
+    this->pos = pos;
+    this->worldUp = normalize(up);
+    
+    this->updateAngles();
+}
+
+glm::mat4 Camera::viewMatrix() {
+    return glm::lookAt(this->pos, this->pos + this->front, this->up);
+}
+
+glm::vec3 Camera::getPosition() {
+    return this->pos;
+}
+
+glm::vec3 Camera::getFront() {
+    return this->front;    
+}
+
+void Camera::processMovement(GLint key) {
+    switch (key) {
+        case GLFW_KEY_W:
+            this->pos += this->front * movementSpeed;
+            break;
+        case GLFW_KEY_A:
+            this->pos -= this->right * movementSpeed;
+            break;
+        case GLFW_KEY_S:
+            this->pos -= this->front * movementSpeed;
+            break;
+        case GLFW_KEY_D:
+            this->pos += this->right * movementSpeed;
+            break;
+    }
+    
+    this->updateAngles();
+}
+
+void Camera::processCursor(GLfloat x_offset, GLfloat y_offset, GLboolean capPitch) {
+    x_offset *= this->mouseSensitivity;
+    y_offset *= this->mouseSensitivity;
+    
+    this->yaw += x_offset;
+    this->pitch += y_offset;
+    
+    if (capPitch) {
+        if (this->pitch > 89.0f) { this->pitch = 89.0f; }
+        if (this->pitch < -89.0f) { this->pitch = -89.0f; }
+    }
+    
+    this->updateAngles();
+}
+
+void Camera::updateAngles() {
+    glm::vec3 newFront;
+    
+    newFront.x = cos(glm::radians(this->yaw)) * cos(glm::radians(this->pitch));
+    newFront.y = sin(glm::radians(this->pitch));
+    newFront.z = sin(glm::radians(this->yaw)) * cos(glm::radians(this->pitch));
+    
+    this->front = glm::normalize(newFront);
+    this->right = glm::normalize(glm::cross(this->front, this->worldUp));
+    this->up = glm::normalize(glm::cross(this->right, this->front));
+}
---
+++ GFX/Phong lighted sphere/Camera.h
@@ -0,0 +1,30 @@
+#ifndef _CAMERA_H
+#define _CAMERA_H
+
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+const GLfloat YAW = -90.0f;
+const GLfloat PITCH = 0.0f;
+const GLfloat SPEED = 0.05f;
+const GLfloat SENSITIVITY = 0.1f;
+
+class Camera {
+public:
+    Camera(glm::vec3 pos, glm::vec3 target, glm::vec3 up);
+    glm::mat4 viewMatrix();
+    glm::vec3 getPosition();
+    glm::vec3 getFront();
+    void processCursor(GLfloat x_offset, GLfloat y_offset, GLboolean);
+    void processMovement(GLint key);
+private:
+    glm::vec3 pos, front, up, right, worldUp = glm::vec3(0.0f, 1.0f, 0.0f);
+    GLfloat yaw = YAW, pitch = PITCH;
+    GLfloat movementSpeed = SPEED, mouseSensitivity = SENSITIVITY;
+    void updateAngles();
+};
+
+#endif

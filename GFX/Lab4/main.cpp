12da233b7058cc948b250bf800281cf9ebe39796
Алексей Олохтонов <aolo2@yandex.ru>
2017-04-25 09:56:54 +0300

    optimizations + broken resize
    only determine the 'rightness' of polygon once (until it is changed)
    added frame_buffer_callback, but still broken cause [-1, 1]

--- GFX/Lab4/main.cpp
+++ GFX/Lab4/main.cpp
@@ -1,21 +1,11 @@
 #include "Shader/Shader.h"
+#include "Utility/Utils.h"
 
 #include <GLFW/glfw3.h>
-#include <vector>
-
-int WINDOW_WIDTH = 1280;
-int WINDOW_HEIGHT = 720;
-
-bool fill, antialias;
-
-struct Edge {
-    glm::vec2 start;
-    glm::vec2 end;
-};
 
 GLuint CANVAS;
 std::vector<Edge> edges;
-GLubyte *data;
+bool fill, antialias, determined, righthand;
 
 inline int sign(int i) { return i == 0 ? 0 : (i > 0 ? 1 : -1); }
 void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode);
@@ -37,9 +27,9 @@ int main() {
 
     glfwSetKeyCallback(window, key_callback);
     glfwSetMouseButtonCallback(window, mouse_button_callback);
-    // glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    data = (GLubyte *) malloc((WINDOW_WIDTH + 1) * (WINDOW_HEIGHT + 1) * 3 * sizeof(GLubyte));
+    data = (GLubyte *) malloc(WINDOW_WIDTH * WINDOW_HEIGHT * 3 * sizeof(GLubyte));
     memset(data, 0x00, WINDOW_WIDTH * WINDOW_HEIGHT * 3 * sizeof(GLubyte));
 
     // =======QUAD=============
@@ -115,244 +105,173 @@ int main() {
     return 0;
 }
 
-void color_pixel_at(int x, int y, float brightness = 8.0f) {
-    x += WINDOW_WIDTH / 2;
-    y += WINDOW_HEIGHT / 2;
-
-    int color = (int) (0xFF / 8.0f * brightness);
-    
-    data[WINDOW_WIDTH * y * 3 + x * 3 + 0] = color;
-    data[WINDOW_WIDTH * y * 3 + x * 3 + 1] = color;
-    data[WINDOW_WIDTH * y * 3 + x * 3 + 2] = color;
-}
-
-void invert_pixel_at(int x, int y) {
-    x += WINDOW_WIDTH / 2;
-    y += WINDOW_HEIGHT / 2;
-    
-    data[WINDOW_WIDTH * y * 3 + x * 3 + 0] ^= 0xFF;
-    data[WINDOW_WIDTH * y * 3 + x * 3 + 1] ^= 0xFF;
-    data[WINDOW_WIDTH * y * 3 + x * 3 + 2] ^= 0xFF;
-}
-
-
-bool pixel_at_filled(int x, int y) {
-    x += WINDOW_WIDTH / 2;
-    y += WINDOW_HEIGHT / 2;
-
-    return data[WINDOW_WIDTH * y * 3 + x * 3] == 0xFF;
-}
-
-int intersection_x(const Edge &edge, float y) {
-    float k, b;
-
-    if (y > std::max(edge.start.y, edge.end.y) || y < std::min(edge.start.y, edge.end.y)) {
-        return -1.0f * WINDOW_WIDTH;
-    }
-
-    float dx = edge.end.x - edge.start.x;
-    float dy = edge.end.y - edge.start.y;
-
-    // what if dx == 0?
-    // vertical line
-    if (dx == 0) {
-        return edge.end.x;
-    }
-    
-    k = dy / dx;
-    b = edge.end.y - k * edge.end.x;
-    
-    float x_int = (y - b) / k;
-
-    // the left-most pixel, center of which is to the right of intersection
-    // what if rounded up, but already at the edge of a window?
-    int x = std::min((int) x_int, WINDOW_WIDTH / 2);
-}
-
-int octant_number(int dx, int dy) {
-    int abs_dx = abs(dx);
-    int abs_dy = abs(dy);
-    
-    if (dx > 0 && dy > 0) {
-        return (abs_dx > abs_dy) ? 1 : 2;         
-    } else if (dx < 0 && dy > 0) {
-        return (abs_dy > abs_dx) ? 3 : 4;
-    } else if (dx < 0 && dy < 0) {
-        return (abs_dx > abs_dy) ? 5 : 6;
-    } else {
-        // dx > 0 && dy < 0
-        return (abs_dy > abs_dx) ? 7 : 8;
-    }
-}
-
 void rasterize() {
-    memset(data, 0x00, (WINDOW_WIDTH + 1) * (WINDOW_HEIGHT + 1) * 3 * sizeof(GLubyte));
+    memset(data, 0x00, WINDOW_WIDTH * WINDOW_HEIGHT * 3 * sizeof(GLubyte));
 
-    if (edges.size() >= 3) {
-        // connect first and last vertex
-        edges[0].start = edges.back().end;
+    if (edges.size() < 3) {
+        glBindTexture(GL_TEXTURE_2D, CANVAS);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, WINDOW_WIDTH, WINDOW_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glBindTexture(GL_TEXTURE_2D, 0);
+        
+        return;
+    }
+     
+    // connect first and last vertex
+    edges[0].start = edges.back().end;
 
-        if (fill) {
-            for (float y = WINDOW_HEIGHT / 2 + 0.5f; y > WINDOW_HEIGHT / -2; y--) {
-                for (const Edge &edge : edges) {
-                    int x_int = intersection_x(edge, y);
-                    if (x_int != -1.0f * WINDOW_WIDTH) {
-                        invert_pixel_at(x_int, y - 0.5f);
-                    }
+    if (fill) {
+        // find pairs pixels on each scanning line (step 1)
+        for (float y = WINDOW_HEIGHT / 2 + 0.5f; y > WINDOW_HEIGHT / -2; y--) {
+            for (const Edge &relative_edge : edges) {
+                Edge edge = recalculate(relative_edge);
+                
+                int x_int = intersection_x(edge, y);
+                if (x_int != -1.0f * WINDOW_WIDTH) {
+                    invert_pixel_at(x_int, y - 0.5f);
                 }
             }
-
-            for (int y = WINDOW_HEIGHT / 2; y >= WINDOW_HEIGHT / -2; y--) {
-                bool inside = false;
-                for (int x = WINDOW_WIDTH / -2; x <= WINDOW_WIDTH / 2; x++) {
-                    if (pixel_at_filled(x, y)) {
-                        inside = !inside;
-                    }
-
+        }
+        
+        // fill everything inbetween pairs of pixels (step 2)
+        for (int y = WINDOW_HEIGHT / 2; y > WINDOW_HEIGHT / -2; y--) {
+            bool inside = false;
+            for (int x = WINDOW_WIDTH / -2; x < WINDOW_WIDTH / 2; x++) {
+                if (pixel_at_filled(x, y)) {
+                    inside = !inside;
+                }
                     if (inside) {
-                        color_pixel_at(x, y);
-                    }
-                }
-            }
-        }
-        
-        
-        if (antialias) {
-            // расчитать справа или слева многоугольник
-            // в зависимости от этого инвертировать яркость + не красить пиксели, 
-            /// которые с "неправильной" стороны от границы по брезенхему
-            
-            
-            int I = 8;
-            glm::mat2 rot;
-
-            for (int k = 0; k < edges.size(); k++) {
-                Edge edge = edges[k];
-                int x1 = edge.start.x;
-                int y1 = edge.start.y;
-    
-                int x2 = edge.end.x;
-                int y2 = edge.end.y;
-    
-                if (x1 == x2 && y1 == y2) {
-                    continue;
-                }
-    
-                int octant = octant_number(x2 - x1, y2 - y1);
-                
-                bool inverse_brightness = false;
-                // inverse_brightness = (ocant % 2 != 0);
-                
-                
-                // works if polygon is LEFT from the edge
-                
-                int x, y, dx, dy, s1, s2;
-                float e;
-                
-                x = x1;
-                y = y1;
-    
-                dx = abs(x2 - x1);
-                dy = abs(y2 - y1);
-    
-                glm::vec2 another = (k == edges.size() - 1) ? edges[0].end : edges[k + 1].end;
-                float angle = - atan2((float) (y2 - y1),  (float) (x2 - x1)) + M_PI / 2;
-                
-                rot[0][0] = cos(angle);
-                rot[1][0] = -sin(angle);
-                rot[0][1] = sin(angle);
-                rot[1][1] = cos(angle);
-                
-                float another_x = (rot * another).x;
-                float our_x = (rot * edge.end).x;
-                
-                if (another_x > our_x) {
-                    inverse_brightness = true;
-                }
-    
-                s1 = sign(x2 - x1);
-                s2 = sign(y2 - y1);
-    
-                bool swap = false;
-                if (dy > dx){
-                    std::swap(dx, dy);
-                    swap = true;
-                }
-                
-                float m = ((float) I * dy) / dx;
-                float w = I - m;
-                e = I / 2.0f;
-                
-                color_pixel_at(x, y, m / 2);
-                for (int i = 1; i <= dx; i++) {
-                    if (e < w) {
-                        swap ? y += s2 : x += s1;
-                        e += m;
-                    } else {
-                        if (swap) {
-                            y += s2;
-                            x += s1;
-                        } else {
-                            y += s2;
-                            x += s1;
-                        }
-                        e -= w;
-                        
-                    }
-                    
-                    if (inverse_brightness) {
-                        color_pixel_at(x, y, (octant % 2 != 0) ? e : 8.0f - e);
-                    } else {
-                        color_pixel_at(x, y, (octant % 2 != 0) ? 8.0f - e : e);
-                    }
-            }
-        }
-    } else {
-            for (Edge edge : edges) {
-                int x1 = edge.start.x;
-                int y1 = edge.start.y;
-    
-                int x2 = edge.end.x;
-                int y2 = edge.end.y;
-    
-                if (x1 == x2 && y1 == y2) {
-                    continue;
-                }
-    
-                int x, y, dx, dy, e, s1, s2;
-                x = x1;
-                y = y1;
-    
-                dx = abs(x2 - x1);
-                dy = abs(y2 - y1);
-    
-                s1 = sign(x2 - x1);
-                s2 = sign(y2 - y1);
-    
-                bool swap = false;
-                if (dy > dx){
-                    std::swap(dx, dy);
-                    swap = true;
-                }
-    
-                e = 2 * dy - dx;
-    
-                for (int j = 1; j <= dx; j++) {
-                    color_pixel_at(x, y);
-    
-                    while (e >= 0) {
-                        swap ? x += s1 : y += s2;
-                        e -= 2 * dx;
-                    }
-    
-                    swap ? y += s2 : x += s1;
-                    e += 2 * dy;
+                    color_pixel_at(x, y, 8.0f);
                 }
             }
         }
     }
     
     
+    if (antialias) {
+        int I = 8;
+        glm::mat2 rot;
+        
+        if (!determined) {
+            Edge e1 = edges[0];
+            float dx = (float) (e1.end.x - e1.start.x);
+            float dy = (float) (e1.end.y - e1.start.y);
+            
+            float angle = - atan2(dy, dx) + M_PI / 2;
+            
+            rot[0][0] = cos(angle);
+            rot[1][0] = -sin(angle);
+            rot[0][1] = sin(angle);
+            rot[1][1] = cos(angle);
+            
+            glm::vec2 e2 = edges[1].end;
+            
+            float edge_end_x = (rot * e1.end).x;
+            float another_x = (rot * e2).x;
+            
+            righthand = (another_x > edge_end_x);
+            determined = true;
+        }
+        
+        for (const Edge &relative_edge : edges) {
+            Edge edge = recalculate(relative_edge);
+            
+            int x1 = edge.start.x;
+            int y1 = edge.start.y;
+
+            int x2 = edge.end.x;
+            int y2 = edge.end.y;
+
+            int octant = octant_number(x2 - x1, y2 - y1);
+            
+            int x, y, dx, dy, s1, s2;
+            float e;
+            
+            x = x1;
+            y = y1;
+
+            dx = abs(x2 - x1);
+            dy = abs(y2 - y1);
+
+            s1 = sign(x2 - x1);
+            s2 = sign(y2 - y1);
+
+            bool swap = false;
+            if (dy > dx){
+                std::swap(dx, dy);
+                swap = true;
+            }
+            
+            float m = ((float) I * dy) / dx;
+            float w = I - m;
+            e = I / 2.0f;
+            
+            color_pixel_at(x, y, m / 2);
+            for (int i = 1; i <= dx; i++) {
+                if (e < w) {
+                    swap ? y += s2 : x += s1;
+                    e += m;
+                } else {
+                    if (swap) {
+                        y += s2;
+                        x += s1;
+                    } else {
+                        y += s2;
+                        x += s1;
+                    }
+                    e -= w;
+                    
+                }
+                
+                if (righthand) {
+                    color_pixel_at(x, y, (octant % 2 != 0) ? e : 8.0f - e);
+                } else {
+                    color_pixel_at(x, y, (octant % 2 != 0) ? 8.0f - e : e);
+                }
+            }
+        }
+        
+    } else { // if !antialias
+        for (const Edge &relative_edge : edges) {
+            Edge edge = recalculate(relative_edge);
+            
+            int x1 = edge.start.x;
+            int y1 = edge.start.y;
+
+            int x2 = edge.end.x;
+            int y2 = edge.end.y;
+
+            int x, y, dx, dy, e, s1, s2;
+            x = x1;
+            y = y1;
+
+            dx = abs(x2 - x1);
+            dy = abs(y2 - y1);
+
+            s1 = sign(x2 - x1);
+            s2 = sign(y2 - y1);
+
+            bool swap = false;
+            if (dy > dx){
+                std::swap(dx, dy);
+                swap = true;
+            }
+
+            e = 2 * dy - dx;
+
+            for (int j = 1; j <= dx; j++) {
+                color_pixel_at(x, y, 8.0f);
+
+                while (e >= 0) {
+                    swap ? x += s1 : y += s2;
+                    e -= 2 * dx;
+                }
+
+                swap ? y += s2 : x += s1;
+                e += 2 * dy;
+            }
+        }
+    }
+
     glBindTexture(GL_TEXTURE_2D, CANVAS);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, WINDOW_WIDTH, WINDOW_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
     glBindTexture(GL_TEXTURE_2D, 0);
@@ -385,7 +304,9 @@ void key_callback(GLFWwindow *window, int key, int scancode, int action, int mod
 
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    data = (GLubyte *) realloc(data, width * height * 3 * sizeof(GLubyte));
     WINDOW_WIDTH = width, WINDOW_HEIGHT = height;
+    rasterize();
     glViewport(0, 0, width, height);
 }
 
@@ -394,18 +315,27 @@ void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
     if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
         double x, y;
         glfwGetCursorPos(window, &x, &y);
-
+        // (0, 0) to (WINDOW_WIDTH - 1, WINDOW_HEIGHT - 1);
+        // std::cout << x << " " << y << std::endl;
+        glm::vec2 cursor_pos = px_to_screen(x, y);
+        
         Edge edge;
         if (edges.size() > 0) {
             edge.start = edges.back().end;
-            edge.end = glm::vec2(x - WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 - y);
-            if (edge.start != edge.end) edges.push_back(edge);
+            edge.end = cursor_pos;
+            if (edge.start != edge.end) {
+                edges.push_back(edge);
+                determined = false;
+                rasterize();
+            }
         } else {
             edge.start = glm::vec2(0, 0);
-            edge.end = glm::vec2(x - WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 - y);
-            if (edge.start != edge.end) edges.push_back(edge);
+            edge.end = cursor_pos;
+            if (edge.start != edge.end) {
+                edges.push_back(edge);
+                determined = false;
+                rasterize();
+            }
         }
-
-        rasterize();
     }
 }
